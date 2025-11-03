dec738b667fddde1004622e6eec686441f202422
Muhammad Yousuf Khan <khanyousuf2144@gmail.com>
2025-11-03 14:33:47 +0530

    feat: add CORS support and MongoDB connection setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import {connectDB} from "./config/db";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+connectDB();
 
 app.get("/", (_req, res) => {
   res.send("Server running with TypeScript!");
